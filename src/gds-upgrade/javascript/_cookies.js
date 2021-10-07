739a2ab1c91155536b80fa01580b707773336ab7
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2021-10-07 15:23:56 +0100

    NExt iteration

--- src/gds-upgrade/javascript/_cookies.js
+++ src/gds-upgrade/javascript/_cookies.js
@@ -2,11 +2,61 @@ var COOKIE_NAMES = {
   USER_BANNER_LAST_SEEN: 'user_banner_last_seen'
 };
 
+var GOVUK_COOKIE_OPTIONS = {
+  expires: 365, // days
+  secure: true,
+  domain: '.education.gov.uk'
+};
+
+var GovUKCookie = {
+  getRaw: function (name) {
+    if (!window.Cookies) {
+      return;
+    }
+
+    return window.Cookies.get(name);
+  },
+  get: function (name) {
+    if (!window.Cookies) {
+      return;
+    }
+
+    var value = window.Cookies.get(name);
+    if (value) {
+      return JSON.parse(value);
+    }
+    return value;
+  },
+  set: function (name, value) {
+    if (!window.Cookies) {
+      return;
+    }
+
+    return window.Cookies.set(
+      name,
+      value,
+      GOVUK_COOKIE_OPTIONS
+    );
+  },
+  remove: function (name) {
+    if (!window.Cookies) {
+      return;
+    }
+
+    return window.Cookies.remove(
+      name,
+      GOVUK_COOKIE_OPTIONS
+    );
+  }
+};
+
 // function that will remove any existing tracking cookies on page load
 (function () {
   var cookies = document.cookie.split("; ");
-  console.log(cookies);
   for (var c = 0; c < cookies.length; c++) {
+    // skip if it is the review users banner
+    if(cookies[c].indexOf(COOKIE_NAMES.USER_BANNER_LAST_SEEN) !== -1) {
+      console.log(cookies[c]);
       var d = window.location.hostname.split(".");
       while (d.length > 0) {
           var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
@@ -18,8 +68,10 @@ var COOKIE_NAMES = {
           };
           d.shift();
       }
+    }
   }
 })();
+
 
 /**
  * Section to handle review users banner
